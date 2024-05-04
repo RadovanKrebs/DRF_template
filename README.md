8a5eba1d07b35d3dce43df63a140953b49548f84
Radovan Krebs <514294@mail.muni.cz>
2024-05-04 13:13:26 +0200

    Add DRF Boilerplate with Custom user and DevOps setup

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # DRF_template
 Django rest framework template set up with docker compose and a postgresql, database and custom user and admin
+
+## Run management commands
+Use docker compose to run django commands in docker-compose services:
+```
+docker-compose run --rm app sh -c "python manage.py <command-name>"
+```
+
+## Use with pycharm
+Create a dummy venv from the requirements file and set pycharm to use it as python interpreter. This venv will not be used by the app, just for pycharm.
+```
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install -r requirements.txt
+<set the .venv/bin/python as the python interpreter in pycharm>
+```
+
+**Note:** Pycharm should allow to use a python interpreter from docker-compose, but I didn't manage to do it. You can try.
+
+## Configure dockerhub credentials
+
+In dockerhub, log in, go to My Account > Security > New Access Token, and copy the created token.
+
+Open the project created from this template in github, got to Settings > Secrets and variables > Actions, and add following repository secrets:
+
+```
+DOCKERHUB_TOKEN = <token-generated-in-dockerhub>>
+DOCKERHUB_USER = <your-dockerhub-username>
+```
+
---
+++ app/core/models.py
@@ -0,0 +1,53 @@
+"""
+Database models.
+"""
+
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError("User must have an email address.")
+        try:
+            validate_email(email)
+        except ValidationError:
+            raise ValueError(f"User provided with a invalid email address: {email}")
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
